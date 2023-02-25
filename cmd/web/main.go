df859b62b036552c0ed5afc44628c8beea7fa008
Andre Lopez <andre.jl@yahoo.com>
2023-02-24 22:47:40 -0800

    Added command line args for server port and path of static content

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 
@@ -8,25 +9,28 @@ import (
 	"github.com/gofiber/template/html"
 )
 
-const PORT = ":4000"
-
 func main() {
+	// Obtain command line args
+	PORT := flag.String("port", ":4000", "HTTP Network Address")
+	STATIC_PATH := flag.String("static-path", "./ui/static", "Path of static conent to serve")
+	flag.Parse()
+
 	engine := html.New("./ui/html", ".html")
 	app := fiber.New(fiber.Config{
 		Views:       engine,
 		ViewsLayout: "layouts/main",
 	})
 
-	app.Static("/static", "./ui/static", fiber.Static{Browse: true})
+	app.Static("/static", *STATIC_PATH, fiber.Static{Browse: true})
 
 	app.Get("/", home)
 	app.Get("/snippet/view", viewSnippet)
 	app.Post("/snippet/create", createSnippet)
 
-	log.Println("Starting on server", PORT)
+	log.Println("Starting on server", *PORT)
 	app.Use(func(c *fiber.Ctx) error {
 		return c.SendStatus(http.StatusNotFound)
 	})
 
-	log.Fatal(app.Listen(PORT))
+	log.Fatal(app.Listen(*PORT))
 }
