ab0d833a8e79fd787d2000f75f9e1529e2b99f39
Andre Lopez <andre.jl@yahoo.com>
2023-02-26 00:02:34 -0800

    Completed createSnippet handler and snipper Insert method

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -30,6 +30,24 @@ func (app *application) viewSnippet(c *fiber.Ctx) error {
 }
 
 func (app *application) createSnippet(c *fiber.Ctx) error {
-	c.SendString("Create route from snippetbox")
+	body := struct {
+		title   string `json:"title"`
+		content string `json:"content"`
+		expires int    `json:"expires"`
+	}{}
+
+	if err := c.BodyParser(&body); err != nil {
+		return c.Status(fiber.StatusUnprocessableEntity).JSON(fiber.Map{
+			"errors": err.Error(),
+		})
+	}
+
+	id, err := app.snippets.Insert(body.title, body.content, body.expires)
+	if err != nil {
+		app.serverError(c, err)
+		return err
+	}
+
+	c.Redirect(fmt.Sprintf("/snippet/view?id=%d", id), fiber.StatusSeeOther)
 	return nil
 }
