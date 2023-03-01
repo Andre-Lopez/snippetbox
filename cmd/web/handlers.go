fd530dc4eae339308f2cb2d9dc4029e0cecbe1de
Andre Lopez <andre.jl@yahoo.com>
2023-02-28 22:35:57 -0800

    Added validation to snippet creation form with rendered error messages

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -4,7 +4,9 @@ import (
 	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 	"time"
+	"unicode/utf8"
 
 	"github.com/Andre-Lopez/snippetbox/internal/models"
 	"github.com/gofiber/fiber/v2"
@@ -44,28 +46,47 @@ func (app *application) viewSnippet(c *fiber.Ctx) error {
 }
 
 func (app *application) createSnippetPost(c *fiber.Ctx) error {
-	body := struct {
-		title   string `json:"title"`
-		content string `json:"content"`
-		expires int    `json:"expires"`
-	}{}
-
-	if err := c.BodyParser(&body); err != nil {
-		return c.Status(fiber.StatusUnprocessableEntity).JSON(fiber.Map{
-			"errors": err.Error(),
-		})
+	title := c.FormValue("title")
+	content := c.FormValue("content")
+	expires, err := strconv.Atoi(c.FormValue("expires"))
+	if err != nil {
+		app.clientError(c, fiber.StatusBadRequest)
+		return err
 	}
 
-	id, err := app.snippets.Insert(body.title, body.content, body.expires)
+	fieldErrors := make(map[string]string)
+
+	// Validate title
+	if strings.TrimSpace(title) == "" {
+		fieldErrors["title"] = "This field cannot be empty"
+	} else if utf8.RuneCountInString(title) > 100 {
+		fieldErrors["title"] = "This field cannot be longer than 100 characters"
+	}
+
+	// Validate content
+	if strings.TrimSpace(content) == "" {
+		fieldErrors["content"] = "This field cannot be empty"
+	}
+
+	if expires != 1 && expires != 7 && expires != 365 {
+		fieldErrors["expires"] = "This field must have a value of 1, 7, or 365"
+	}
+
+	// Re-serve the creation form with displayed errors
+	if len(fieldErrors) > 0 {
+		return c.Render("create", fiber.Map{"title": title, "content": content, "expires": expires, "errors": fieldErrors})
+	}
+
+	id, err := app.snippets.Insert(title, content, expires)
 	if err != nil {
 		app.serverError(c, err)
 		return err
 	}
 
-	c.Redirect(fmt.Sprintf("/snippet/view?id=%d", id), fiber.StatusSeeOther)
+	c.Redirect(fmt.Sprintf("/snippet/view/%d", id), fiber.StatusSeeOther)
 	return nil
 }
 
 func (app *application) createSnippet(c *fiber.Ctx) error {
-	return c.Render("create", fiber.Map{})
+	return c.Render("create", fiber.Map{"expires": 365})
 }
